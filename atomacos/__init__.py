aa2f98cb0fb6364f388218cd1900a3d59670cabb
Davee Nguyen <davee@daveenguyen.com>
2019-03-12 17:09:46 -0700

    Match old init

--- atomacos/__init__.py
+++ atomacos/__init__.py
@@ -1,2 +1,25 @@
 """Automated Testing on macOS"""
+# flake8: noqa: F401
 __version__ = "0.1.0.dev0"
+
+from atomacos import a11y
+from atomacos import errors
+from atomacos.AXClasses import NativeUIElement
+from atomacos.Clipboard import Clipboard
+from atomacos.Prefs import Prefs
+
+Error = errors.AXError
+ErrorAPIDisabled = errors.AXErrorAPIDisabled
+ErrorInvalidUIElement = errors.AXErrorInvalidUIElement
+ErrorCannotComplete = errors.AXErrorCannotComplete
+ErrorUnsupported = errors.AXErrorUnsupported
+ErrorNotImplemented = errors.AXErrorNotImplemented
+
+getAppRefByLocalizedName = NativeUIElement.getAppRefByLocalizedName
+terminateAppByBundleId = NativeUIElement.terminateAppByBundleId
+launchAppByBundlePath = NativeUIElement.launchAppByBundlePath
+setSystemWideTimeout = NativeUIElement.setSystemWideTimeout
+getAppRefByBundleId = NativeUIElement.getAppRefByBundleId
+launchAppByBundleId = NativeUIElement.launchAppByBundleId
+getFrontmostApp = NativeUIElement.getFrontmostApp
+getAppRefByPid = NativeUIElement.getAppRefByPid
