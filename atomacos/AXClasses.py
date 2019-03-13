903f82c8dae48f9d7fc964795aa767c49a3b1515
Davee Nguyen <davee@daveenguyen.com>
2019-03-12 21:43:53 -0700

    Use pid property

--- atomacos/AXClasses.py
+++ atomacos/AXClasses.py
@@ -742,7 +742,7 @@ class BaseAXUIElement(a11y.AXUIElement):
     def _activate(self):
         """Activate the application (bringing menus and windows forward)."""
         ra = AppKit.NSRunningApplication
-        app = ra.runningApplicationWithProcessIdentifier_(self._getPid())
+        app = ra.runningApplicationWithProcessIdentifier_(self.pid)
         # NSApplicationActivateAllWindows | NSApplicationActivateIgnoringOtherApps
         # == 3 - PyObjC in 10.6 does not expose these constants though so I have
         # to use the int instead of the symbolic names
