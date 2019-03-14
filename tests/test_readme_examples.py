35e49fc003931fa7c0e13139aed1efd3d62178ac
Davee Nguyen <davee@daveenguyen.com>
2019-03-13 17:32:26 -0700

    Move fixtures to conftest and update tests

--- tests/test_readme_examples.py
+++ tests/test_readme_examples.py
@@ -1,66 +1,56 @@
-import atomacos as atomac
+import pytest
+
+from atomacos import NativeUIElement
+from atomacos.a11y import axenabled
 
 
-def test_launch_app_by_bundle_id():
-    atomac.launchAppByBundleId("com.apple.Automator")
+def test_basic_automator_app_ref(automator_app):
+    assert isinstance(automator_app, NativeUIElement)
 
 
-def test_get_app_ref_by_bundle_id():
-    atomac.launchAppByBundleId("com.apple.Automator")
-    automator = atomac.getAppRefByBundleId("com.apple.Automator")
-    assert isinstance(automator, atomac.NativeUIElement)
+@pytest.mark.skipif(not axenabled(), reason="Accessibility Permission Needed")
+def test_find_object(automator_app):
+    window = automator_app.windows()[0]
+    assert u"Untitled" in window.AXTitle
 
 
-def test_find_object():
-    atomac.launchAppByBundleId("com.apple.Automator")
-    automator = atomac.getAppRefByBundleId("com.apple.Automator")
-    window = automator.windows()[0]
-    assert window.AXTitle == u"Untitled"
-
-
-def test_find_sheet_shortcut():
-    atomac.launchAppByBundleId("com.apple.Automator")
-    automator = atomac.getAppRefByBundleId("com.apple.Automator")
-    window = automator.windows()[0]
+@pytest.mark.skipif(not axenabled(), reason="Accessibility Permission Needed")
+def test_find_sheet_shortcut(automator_app):
+    window = automator_app.windows()[0]
     sheet1 = window.sheets()[0]
-    sheet2 = automator.sheetsR()[0]
+    sheet2 = automator_app.sheetsR()[0]
     assert sheet1 == sheet2
 
 
-def test_search_method():
-    atomac.launchAppByBundleId("com.apple.Automator")
-    automator = atomac.getAppRefByBundleId("com.apple.Automator")
-    window = automator.windows()[0]
+@pytest.mark.skipif(not axenabled(), reason="Accessibility Permission Needed")
+def test_search_method(automator_app):
+    window = automator_app.windows()[0]
     sheet = window.sheets()[0]
     close = sheet.buttons("Close")[0]
-    assert isinstance(close, atomac.NativeUIElement)
+    assert isinstance(close, NativeUIElement)
 
 
-def test_search_by_axattributes():
-    atomac.launchAppByBundleId("com.apple.Automator")
-    automator = atomac.getAppRefByBundleId("com.apple.Automator")
-    window = automator.windows()[0]
+@pytest.mark.skipif(not axenabled(), reason="Accessibility Permission Needed")
+def test_search_by_axattributes(automator_app):
+    window = automator_app.windows()[0]
     sheet = window.sheets()[0]
     close = sheet.findFirst(AXRole="AXButton", AXTitle="Close")
-    assert isinstance(close, atomac.NativeUIElement)
+    assert isinstance(close, NativeUIElement)
 
 
-def test_get_list_of_attributes():
-    atomac.launchAppByBundleId("com.apple.Automator")
-    automator = atomac.getAppRefByBundleId("com.apple.Automator")
-    close = automator.findFirstR(AXRole="AXButton", AXTitle="Close")
+@pytest.mark.skipif(not axenabled(), reason="Accessibility Permission Needed")
+def test_get_list_of_attributes(automator_app):
+    close = automator_app.findFirstR(AXRole="AXButton", AXTitle="Close")
     assert len(close.getAttributes()) > 0
 
 
-def test_get_list_of_acctions():
-    atomac.launchAppByBundleId("com.apple.Automator")
-    automator = atomac.getAppRefByBundleId("com.apple.Automator")
-    close = automator.findFirstR(AXRole="AXButton", AXTitle="Close")
+@pytest.mark.skipif(not axenabled(), reason="Accessibility Permission Needed")
+def test_get_list_of_actions(automator_app):
+    close = automator_app.findFirstR(AXRole="AXButton", AXTitle="Close")
     assert len(close.getActions()) > 0
 
 
-def test_perform_action():
-    atomac.launchAppByBundleId("com.apple.Automator")
-    automator = atomac.getAppRefByBundleId("com.apple.Automator")
-    close = automator.findFirstR(AXRole="AXButton", AXTitle="Close")
+@pytest.mark.skipif(not axenabled(), reason="Accessibility Permission Needed")
+def test_perform_action(automator_app):
+    close = automator_app.findFirstR(AXRole="AXButton", AXTitle="Close")
     close.Press()
