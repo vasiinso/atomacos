b8c9090943de3fad29534ec51deb7e4b6fad6f2d
Davee Nguyen <davee@daveenguyen.com>
2019-03-14 16:11:17 -0700

    Add test for some package functions

---
+++ tests/test_package_functions.py
@@ -0,0 +1,28 @@
+import atomacos
+
+
+def test_get_app_ref_by_localized_name(finder_app):
+    finder = atomacos.getAppRefByLocalizedName("Finder")
+    assert finder == finder_app
+
+
+def test_launch_app_by_bundle_path():
+    atomacos.launchAppByBundlePath("/Applications/Calculator.app")
+    automator = atomacos.getAppRefByLocalizedName("Calculator")
+    assert automator.pid != 0
+
+
+def test_set_systemwide_timeout():
+    atomacos.setSystemWideTimeout(0)
+
+
+def test_get_app_by_bundle_id(finder_app):
+    bid = finder_app.getBundleId()
+    bybid = atomacos.getAppRefByBundleId(bid)
+    assert bybid == finder_app
+
+
+def test_get_app_by_pid(finder_app):
+    pid = finder_app.pid
+    app = atomacos.getAppRefByPid(pid)
+    assert app == finder_app
