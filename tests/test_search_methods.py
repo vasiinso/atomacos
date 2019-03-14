a5f9d95704ecd0c00f21ee23dedbcd434467285b
Davee Nguyen <davee@daveenguyen.com>
2019-03-14 14:15:55 -0700

    Add tests for search methods

--- tests/test_search_methods.py
+++ tests/test_search_methods.py
@@ -1,37 +1,71 @@
 import atomacos
 
 
-def test_convenienceMatch(monkeypatch):
+def test_search_method_for_roles(monkeypatch):
     sut = atomacos.NativeUIElement()
-    monkeypatch.setattr(sut, "_convenienceMatch", lambda *args: args)
+    monkeypatch.setattr(sut, "_findAll", lambda **kwargs: kwargs.items())
 
-    assert sut.textAreas(1) == ("AXTextArea", "AXTitle", 1)
-    assert sut.textFields(3) == ("AXTextField", "AXRoleDescription", 3)
-    assert sut.buttons("a") == ("AXButton", "AXTitle", "a")
-    assert sut.windows() == ("AXWindow", "AXTitle", None)
-    assert sut.sheets() == ("AXSheet", "AXDescription", None)
-    assert sut.staticTexts() == ("AXStaticText", "AXValue", None)
-    assert sut.genericElements() == ("AXGenericElement", "AXValue", None)
-    assert sut.groups() == ("AXGroup", "AXRoleDescription", None)
-    assert sut.radioButtons() == ("AXRadioButton", "AXTitle", None)
-    assert sut.popUpButtons() == ("AXPopUpButton", "AXTitle", None)
-    assert sut.rows() == ("AXRow", "AXTitle", None)
-    assert sut.sliders() == ("AXSlider", "AXValue", None)
+    assert ("AXRole", "AXTextField") in sut.textFields()
+    assert ("AXRole", "AXButton") in sut.buttons()
+    assert ("AXRole", "AXWindow") in sut.windows()
+    assert ("AXRole", "AXSheet") in sut.sheets()
+    assert ("AXRole", "AXStaticText") in sut.staticTexts()
+    assert ("AXRole", "AXGenericElement") in sut.genericElements()
+    assert ("AXRole", "AXGroup") in sut.groups()
+    assert ("AXRole", "AXRadioButton") in sut.radioButtons()
+    assert ("AXRole", "AXPopUpButton") in sut.popUpButtons()
+    assert ("AXRole", "AXRow") in sut.rows()
+    assert ("AXRole", "AXSlider") in sut.sliders()
+    assert ("AXRole", "AXSlider") in sut.sliders()
 
 
-def test_recursive(monkeypatch):
+def test_search_method_for_roles_recursive(monkeypatch):
     sut = atomacos.NativeUIElement()
-    monkeypatch.setattr(sut, "_convenienceMatchR", lambda *args: args)
+    monkeypatch.setattr(sut, "_findAllR", lambda **kwargs: kwargs.items())
 
-    assert sut.textAreasR(2) == ("AXTextArea", "AXTitle", 2)
-    assert sut.textFieldsR(4) == ("AXTextField", "AXRoleDescription", 4)
-    assert sut.buttonsR("a") == ("AXButton", "AXTitle", "a")
-    assert sut.windowsR() == ("AXWindow", "AXTitle", None)
-    assert sut.sheetsR() == ("AXSheet", "AXDescription", None)
-    assert sut.staticTextsR() == ("AXStaticText", "AXValue", None)
-    assert sut.genericElementsR() == ("AXGenericElement", "AXValue", None)
-    assert sut.groupsR() == ("AXGroup", "AXRoleDescription", None)
-    assert sut.radioButtonsR() == ("AXRadioButton", "AXTitle", None)
-    assert sut.popUpButtonsR() == ("AXPopUpButton", "AXTitle", None)
-    assert sut.rowsR() == ("AXRow", "AXTitle", None)
-    assert sut.slidersR() == ("AXSlider", "AXValue", None)
+    assert ("AXRole", "AXTextField") in sut.textFieldsR()
+    assert ("AXRole", "AXButton") in sut.buttonsR()
+    assert ("AXRole", "AXWindow") in sut.windowsR()
+    assert ("AXRole", "AXSheet") in sut.sheetsR()
+    assert ("AXRole", "AXStaticText") in sut.staticTextsR()
+    assert ("AXRole", "AXGenericElement") in sut.genericElementsR()
+    assert ("AXRole", "AXGroup") in sut.groupsR()
+    assert ("AXRole", "AXRadioButton") in sut.radioButtonsR()
+    assert ("AXRole", "AXPopUpButton") in sut.popUpButtonsR()
+    assert ("AXRole", "AXRow") in sut.rowsR()
+    assert ("AXRole", "AXSlider") in sut.slidersR()
+    assert ("AXRole", "AXSlider") in sut.slidersR()
+
+
+def test_search_extra_attributes(monkeypatch):
+    sut = atomacos.NativeUIElement()
+    monkeypatch.setattr(sut, "_findAll", lambda **kwargs: kwargs.items())
+
+    assert ("AXRoleDescription", 1) in sut.textFields(1)
+    assert ("AXTitle", 1) in sut.buttons(1)
+    assert ("AXTitle", 1) in sut.windows(1)
+    assert ("AXDescription", 1) in sut.sheets(1)
+    assert ("AXValue", 1) in sut.staticTexts(1)
+    assert ("AXValue", 1) in sut.genericElements(1)
+    assert ("AXRoleDescription", 1) in sut.groups(1)
+    assert ("AXTitle", 1) in sut.radioButtons(1)
+    assert ("AXTitle", 1) in sut.popUpButtons(1)
+    assert ("AXTitle", 1) in sut.rows(1)
+    assert ("AXValue", 1) in sut.sliders(1)
+
+
+def test_search_extra_attributes_recursive(monkeypatch):
+    sut = atomacos.NativeUIElement()
+    monkeypatch.setattr(sut, "_findAllR", lambda **kwargs: kwargs.items())
+
+    assert ("AXRoleDescription", 1) in sut.textFieldsR(1)
+    assert ("AXTitle", 1) in sut.buttonsR(1)
+    assert ("AXTitle", 1) in sut.windowsR(1)
+    assert ("AXDescription", 1) in sut.sheetsR(1)
+    assert ("AXValue", 1) in sut.staticTextsR(1)
+    assert ("AXValue", 1) in sut.genericElementsR(1)
+    assert ("AXRoleDescription", 1) in sut.groupsR(1)
+    assert ("AXTitle", 1) in sut.radioButtonsR(1)
+    assert ("AXTitle", 1) in sut.popUpButtonsR(1)
+    assert ("AXTitle", 1) in sut.rowsR(1)
+    assert ("AXValue", 1) in sut.slidersR(1)
