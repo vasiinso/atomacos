9d14084b7aea9726f954cb007ba71fd6274f08df
Davee Nguyen <davee@daveenguyen.com>
2019-03-14 14:24:30 -0700

    Extract search methods into its own class

--- atomacos/AXClasses.py
+++ atomacos/AXClasses.py
@@ -22,23 +22,7 @@ from atomacos import AXCallbacks
 from atomacos._base_ax_ui_element import BaseAXUIElement
 
 
-class NativeUIElement(BaseAXUIElement):
-    """NativeUIElement class - expose the accessibility API in the simplest,
-    most natural way possible.
-    """
-
-    def getAttributes(self):
-        """Get a list of the attributes available on the element."""
-        return self.ax_attributes
-
-    def getActions(self):
-        """Return a list of the actions available on the element."""
-        return self._getActions()
-
-    def setString(self, attribute, string):
-        """Set the specified attribute to the specified string."""
-        return self._setString(attribute, string)
-
+class SearchMethodsMixin(object):
     def findFirst(self, **kwargs):
         """Return the first object that matches the criteria."""
         return self._findFirst(**kwargs)
@@ -58,6 +42,142 @@ class NativeUIElement(BaseAXUIElement):
         the specified criteria.
         """
         return self._findAllR(**kwargs)
+
+    def _convenienceMatch(self, role, attr, match):
+        """Method used by role based convenience functions to find a match"""
+        kwargs = {}
+        # If the user supplied some text to search for,
+        # supply that in the kwargs
+        if match:
+            kwargs[attr] = match
+        return self.findAll(AXRole=role, **kwargs)
+
+    def _convenienceMatchR(self, role, attr, match):
+        """Method used by role based convenience functions to find a match"""
+        kwargs = {}
+        # If the user supplied some text to search for,
+        # supply that in the kwargs
+        if match:
+            kwargs[attr] = match
+        return self.findAllR(AXRole=role, **kwargs)
+
+    def textAreas(self, match=None):
+        """Return a list of text areas with an optional match parameter."""
+        return self._convenienceMatch("AXTextArea", "AXTitle", match)
+
+    def textAreasR(self, match=None):
+        """Return a list of text areas with an optional match parameter."""
+        return self._convenienceMatchR("AXTextArea", "AXTitle", match)
+
+    def textFields(self, match=None):
+        """Return a list of textfields with an optional match parameter."""
+        return self._convenienceMatch(
+            "AXTextField", "AXRoleDescription", match
+        )
+
+    def textFieldsR(self, match=None):
+        """Return a list of textfields with an optional match parameter."""
+        return self._convenienceMatchR(
+            "AXTextField", "AXRoleDescription", match
+        )
+
+    def buttons(self, match=None):
+        """Return a list of buttons with an optional match parameter."""
+        return self._convenienceMatch("AXButton", "AXTitle", match)
+
+    def buttonsR(self, match=None):
+        """Return a list of buttons with an optional match parameter."""
+        return self._convenienceMatchR("AXButton", "AXTitle", match)
+
+    def windows(self, match=None):
+        """Return a list of windows with an optional match parameter."""
+        return self._convenienceMatch("AXWindow", "AXTitle", match)
+
+    def windowsR(self, match=None):
+        """Return a list of windows with an optional match parameter."""
+        return self._convenienceMatchR("AXWindow", "AXTitle", match)
+
+    def sheets(self, match=None):
+        """Return a list of sheets with an optional match parameter."""
+        return self._convenienceMatch("AXSheet", "AXDescription", match)
+
+    def sheetsR(self, match=None):
+        """Return a list of sheets with an optional match parameter."""
+        return self._convenienceMatchR("AXSheet", "AXDescription", match)
+
+    def staticTexts(self, match=None):
+        """Return a list of statictexts with an optional match parameter."""
+        return self._convenienceMatch("AXStaticText", "AXValue", match)
+
+    def staticTextsR(self, match=None):
+        """Return a list of statictexts with an optional match parameter"""
+        return self._convenienceMatchR("AXStaticText", "AXValue", match)
+
+    def genericElements(self, match=None):
+        """Return a list of genericelements with an optional match parameter."""
+        return self._convenienceMatch("AXGenericElement", "AXValue", match)
+
+    def genericElementsR(self, match=None):
+        """Return a list of genericelements with an optional match parameter."""
+        return self._convenienceMatchR("AXGenericElement", "AXValue", match)
+
+    def groups(self, match=None):
+        """Return a list of groups with an optional match parameter."""
+        return self._convenienceMatch("AXGroup", "AXRoleDescription", match)
+
+    def groupsR(self, match=None):
+        """Return a list of groups with an optional match parameter."""
+        return self._convenienceMatchR("AXGroup", "AXRoleDescription", match)
+
+    def radioButtons(self, match=None):
+        """Return a list of radio buttons with an optional match parameter."""
+        return self._convenienceMatch("AXRadioButton", "AXTitle", match)
+
+    def radioButtonsR(self, match=None):
+        """Return a list of radio buttons with an optional match parameter."""
+        return self._convenienceMatchR("AXRadioButton", "AXTitle", match)
+
+    def popUpButtons(self, match=None):
+        """Return a list of popup menus with an optional match parameter."""
+        return self._convenienceMatch("AXPopUpButton", "AXTitle", match)
+
+    def popUpButtonsR(self, match=None):
+        """Return a list of popup menus with an optional match parameter."""
+        return self._convenienceMatchR("AXPopUpButton", "AXTitle", match)
+
+    def rows(self, match=None):
+        """Return a list of rows with an optional match parameter."""
+        return self._convenienceMatch("AXRow", "AXTitle", match)
+
+    def rowsR(self, match=None):
+        """Return a list of rows with an optional match parameter."""
+        return self._convenienceMatchR("AXRow", "AXTitle", match)
+
+    def sliders(self, match=None):
+        """Return a list of sliders with an optional match parameter."""
+        return self._convenienceMatch("AXSlider", "AXValue", match)
+
+    def slidersR(self, match=None):
+        """Return a list of sliders with an optional match parameter."""
+        return self._convenienceMatchR("AXSlider", "AXValue", match)
+
+
+class NativeUIElement(SearchMethodsMixin, BaseAXUIElement):
+    """NativeUIElement class - expose the accessibility API in the simplest,
+    most natural way possible.
+    """
+
+    def getAttributes(self):
+        """Get a list of the attributes available on the element."""
+        return self.ax_attributes
+
+    def getActions(self):
+        """Return a list of the actions available on the element."""
+        return self._getActions()
+
+    def setString(self, attribute, string):
+        """Set the specified attribute to the specified string."""
+        return self._setString(attribute, string)
 
     def getElementAtPosition(self, coord):
         """Return the AXUIElement at the given coordinates.
@@ -406,121 +526,3 @@ class NativeUIElement(BaseAXUIElement):
         return self.waitFor(
             timeout, "AXFocusedWindowChanged", AXTitle=nextWinName
         )
-
-    def _convenienceMatch(self, role, attr, match):
-        """Method used by role based convenience functions to find a match"""
-        kwargs = {}
-        # If the user supplied some text to search for,
-        # supply that in the kwargs
-        if match:
-            kwargs[attr] = match
-        return self.findAll(AXRole=role, **kwargs)
-
-    def _convenienceMatchR(self, role, attr, match):
-        """Method used by role based convenience functions to find a match"""
-        kwargs = {}
-        # If the user supplied some text to search for,
-        # supply that in the kwargs
-        if match:
-            kwargs[attr] = match
-        return self.findAllR(AXRole=role, **kwargs)
-
-    def textAreas(self, match=None):
-        """Return a list of text areas with an optional match parameter."""
-        return self._convenienceMatch("AXTextArea", "AXTitle", match)
-
-    def textAreasR(self, match=None):
-        """Return a list of text areas with an optional match parameter."""
-        return self._convenienceMatchR("AXTextArea", "AXTitle", match)
-
-    def textFields(self, match=None):
-        """Return a list of textfields with an optional match parameter."""
-        return self._convenienceMatch(
-            "AXTextField", "AXRoleDescription", match
-        )
-
-    def textFieldsR(self, match=None):
-        """Return a list of textfields with an optional match parameter."""
-        return self._convenienceMatchR(
-            "AXTextField", "AXRoleDescription", match
-        )
-
-    def buttons(self, match=None):
-        """Return a list of buttons with an optional match parameter."""
-        return self._convenienceMatch("AXButton", "AXTitle", match)
-
-    def buttonsR(self, match=None):
-        """Return a list of buttons with an optional match parameter."""
-        return self._convenienceMatchR("AXButton", "AXTitle", match)
-
-    def windows(self, match=None):
-        """Return a list of windows with an optional match parameter."""
-        return self._convenienceMatch("AXWindow", "AXTitle", match)
-
-    def windowsR(self, match=None):
-        """Return a list of windows with an optional match parameter."""
-        return self._convenienceMatchR("AXWindow", "AXTitle", match)
-
-    def sheets(self, match=None):
-        """Return a list of sheets with an optional match parameter."""
-        return self._convenienceMatch("AXSheet", "AXDescription", match)
-
-    def sheetsR(self, match=None):
-        """Return a list of sheets with an optional match parameter."""
-        return self._convenienceMatchR("AXSheet", "AXDescription", match)
-
-    def staticTexts(self, match=None):
-        """Return a list of statictexts with an optional match parameter."""
-        return self._convenienceMatch("AXStaticText", "AXValue", match)
-
-    def staticTextsR(self, match=None):
-        """Return a list of statictexts with an optional match parameter"""
-        return self._convenienceMatchR("AXStaticText", "AXValue", match)
-
-    def genericElements(self, match=None):
-        """Return a list of genericelements with an optional match parameter."""
-        return self._convenienceMatch("AXGenericElement", "AXValue", match)
-
-    def genericElementsR(self, match=None):
-        """Return a list of genericelements with an optional match parameter."""
-        return self._convenienceMatchR("AXGenericElement", "AXValue", match)
-
-    def groups(self, match=None):
-        """Return a list of groups with an optional match parameter."""
-        return self._convenienceMatch("AXGroup", "AXRoleDescription", match)
-
-    def groupsR(self, match=None):
-        """Return a list of groups with an optional match parameter."""
-        return self._convenienceMatchR("AXGroup", "AXRoleDescription", match)
-
-    def radioButtons(self, match=None):
-        """Return a list of radio buttons with an optional match parameter."""
-        return self._convenienceMatch("AXRadioButton", "AXTitle", match)
-
-    def radioButtonsR(self, match=None):
-        """Return a list of radio buttons with an optional match parameter."""
-        return self._convenienceMatchR("AXRadioButton", "AXTitle", match)
-
-    def popUpButtons(self, match=None):
-        """Return a list of popup menus with an optional match parameter."""
-        return self._convenienceMatch("AXPopUpButton", "AXTitle", match)
-
-    def popUpButtonsR(self, match=None):
-        """Return a list of popup menus with an optional match parameter."""
-        return self._convenienceMatchR("AXPopUpButton", "AXTitle", match)
-
-    def rows(self, match=None):
-        """Return a list of rows with an optional match parameter."""
-        return self._convenienceMatch("AXRow", "AXTitle", match)
-
-    def rowsR(self, match=None):
-        """Return a list of rows with an optional match parameter."""
-        return self._convenienceMatchR("AXRow", "AXTitle", match)
-
-    def sliders(self, match=None):
-        """Return a list of sliders with an optional match parameter."""
-        return self._convenienceMatch("AXSlider", "AXValue", match)
-
-    def slidersR(self, match=None):
-        """Return a list of sliders with an optional match parameter."""
-        return self._convenienceMatchR("AXSlider", "AXValue", match)
