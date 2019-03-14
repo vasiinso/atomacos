0b896a2bdcb55b4357d5d539c3e6d547746c23b7
Davee Nguyen <davee@daveenguyen.com>
2019-03-13 19:05:15 -0700

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 echo $1
+python -m pip install bump2version
 echo $(bumpversion --dry-run --list minor | grep new_version | sed s,'^.*=',,).dev${TRAVIS_BUILD_NUMBER} patch
-flit publish
+python -m flit publish
