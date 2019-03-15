c27d2b79511a7a0a051ddf767503567e5bb3d437
Davee Nguyen <davee@daveenguyen.com>
2019-03-15 03:27:28 -0700

    chore: only release on bump

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 if [[ $TRAVIS_BRANCH = "master" ]]; then
     if [[ $TRAVIS_PULL_REQUEST = "false" ]]; then
-        rm -rf dist/*
+        rm -rfv dist/*
         git config --global user.name "semantic-release (via TravisCI)"
         git config --global user.email "semantic-release@travis"
         pip install python-semantic-release
-        semantic-release version
-        flit publish
-        semantic-release changelog --post
+        if semantic-release version | grep bump; then
+            echo "Deploying"
+            flit publish
+            semantic-release changelog --post
+        else
+            echo "Skipping"
+        fi
     fi
 fi
