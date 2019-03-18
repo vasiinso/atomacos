12e1fdce5e487ae6b3c2ac3b3d9bf6a56557b2ec
Davee Nguyen <davee@daveenguyen.com>
2019-03-18 02:45:45 -0700

    refactor(mixin): extract classes into modules

--- atomacos/mixin/__init__.py
+++ atomacos/mixin/__init__.py
@@ -1,4 +1,4 @@
 # flake8: noqa: F401
-from atomacos.mixin._inputs import KeyboardMouseMixin
+from atomacos.mixin._input import KeyboardMouseMixin
 from atomacos.mixin._search import SearchMethodsMixin
 from atomacos.mixin._wait import WaitForMixin
