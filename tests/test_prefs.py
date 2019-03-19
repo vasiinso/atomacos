982176cbcae7ec5131f633ca627e6182aa03d4a7
Davee Nguyen <davee@daveenguyen.com>
2019-03-19 12:19:22 -0700

    test: add unicode string test and check for string_types instead of str

--- tests/test_prefs.py
+++ tests/test_prefs.py
@@ -1,6 +1,7 @@
 from atomacos import Prefs
+from future.utils import string_types
 
 
 def test_prefs():
     fp = Prefs("com.apple.finder")
-    assert isinstance(fp["GoToField"], str)
+    assert isinstance(fp["GoToField"], string_types)
