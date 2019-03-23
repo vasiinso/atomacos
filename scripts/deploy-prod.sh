eb60f02d302321412d86abcdee2bcdff724d5c21
Davee Nguyen <davee@daveenguyen.com>
2019-03-23 13:57:46 -0700

    ci: install deps in install and build docs in script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -4,7 +4,6 @@ if [[ $TRAVIS_BRANCH = "master" ]]; then
         rm -rfv dist/*
         git config --global user.name "semantic-release (via TravisCI)"
         git config --global user.email "semantic-release@travis"
-        pip install python-semantic-release
         if semantic-release version --noop | grep bumped; then
             echo "Deploying"
             semantic-release publish
