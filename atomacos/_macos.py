ee12beef986baf2f768dc3ce95fd6a316af9966e
Davee Nguyen <davee_nguyen@symantec.com>
2019-08-30 20:35:30 -0700

    feat: retry PAXUIElementCopyAttributeValue on AXErrorFailure

--- atomacos/_macos.py
+++ atomacos/_macos.py
@@ -18,6 +18,7 @@ from ApplicationServices import (
 )
 from atomacos import errors
 from objc import callbackFor
+from tenacity import retry, retry_if_exception_type, stop_after_attempt
 
 PAXObserverCallback = callbackFor(AXObserverCreate)
 
@@ -91,6 +92,11 @@ def PAXObserverRemoveNotification(observer, element, notification):
     errors.check_ax_error(error_code, error_messages)
 
 
+@retry(
+    retry=retry_if_exception_type(errors.AXErrorFailure),
+    reraise=True,
+    stop=stop_after_attempt(5),
+)
 def PAXUIElementCopyAttributeValue(element, attribute):
     """
     Returns the value of an accessibility object's attribute
