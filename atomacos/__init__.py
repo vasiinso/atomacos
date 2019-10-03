d520135ff03ba548140dec07bd1b4ef2a2ae0e5c
semantic-release <semantic-release>
2019-10-03 23:50:20 +0000

    3.2.0
    Automatically generated by python-semantic-release

--- atomacos/__init__.py
+++ atomacos/__init__.py
@@ -1,6 +1,6 @@
 """Automated Testing on macOS"""
 # flake8: noqa: F401
-__version__ = "3.1.0"
+__version__ = "3.2.0"
 
 from atomacos import _a11y, errors, keyboard, mouse
 from atomacos.AXClasses import NativeUIElement
